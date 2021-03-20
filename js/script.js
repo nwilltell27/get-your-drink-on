e63d07cfbece1bcb657a75acbaf5b8e2260d0951
Nathan Tellers <nathantellers@gmail.com>
2021-03-20 10:23:52 -0700

    wip: tests js and adds styling

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/*--- Constant Variables (data that never changes) ---*/
+
+/*--- State Variables (data that changes) ---*/
+
+/*--- Cached Element References (parts of DOM we need to touch) ---*/
+
+/*--- Event Listeners ---*/
+
+/*--- Functions ---*/
+function getData() {
+    $.ajax('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka')
+.then(function(data) { 
+        console.log(data); 
+    }, function(error) {
+        console.log(error);
+    });
+}
