cf77e18c4a126c754efb49a8ee8cb26d5c1c5a75
Nathan Tellers <nathantellers@gmail.com>
2021-03-20 11:14:21 -0700

    feat: gets data after search

--- js/script.js
+++ js/script.js
@@ -1,17 +1,24 @@
 /*--- Constant Variables (data that never changes) ---*/
+const BASE_URL = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=';
 
 /*--- State Variables (data that changes) ---*/
+const $input = $('input[type=text]');
 
 /*--- Cached Element References (parts of DOM we need to touch) ---*/
 
 /*--- Event Listeners ---*/
+$('form').on('submit', handleGetData);
 
 /*--- Functions ---*/
-function getData() {
-    $.ajax('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=')
+function handleGetData(evt) {
+    evt.preventDefault();
+    const userInput = $input.val();
+    $.ajax(BASE_URL + userInput)
         .then(function (data) {
             console.log(data);
         }, function (error) {
             console.log(error);
         });
+    // clears input
+    $input.val(''); 
 }
